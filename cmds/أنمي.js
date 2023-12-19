172f6db1fb2e6ef4438ac13d26ce8e62e69c8a15
HUSSEINHN123 <148639702+HUSSEINHN123@users.noreply.github.com>
2023-12-20 00:09:27 +0100

    Update أنمي.js
    تم اضافة أمر يرسل مقاطع الأنمي

--- cmds/أنمي.js
+++ cmds/أنمي.js
@@ -1,50 +1,19 @@
-const fs = require('fs');
-const https = require('https');
-const {
-  Configuration,
-  OpenAIApi
-} = require("openai");
-const path = require('path');
-const apiKeysPath = path.join(__dirname, '..', 'json', 'api_config.json');
-const apiKeys = JSON.parse(fs.readFileSync(apiKeysPath));
-const openaiApiKey = apiKeys.openai;
+const axios = require('axios');
 
-async function randomAnime(event, api) {
-  // لا يتوقع أي مدخل، لذلك نقوم بتجاهله
-  const configuration = new Configuration({
-    apiKey: openaiApiKey,
-  });
-  const openai = new OpenAIApi(configuration);
+async function getAnimeClip(api, event) {
+  const apiUrl = 'https://jhunapi.mrbaylon4.repl.co/snauzk/?apikey=Marjhunapi';
 
   try {
-    api.sendMessage(" ⏱️ | جاري إرسال صور الأنمي ،يرجى الإنتظار.", event.threadID, event.messageID);
+    const response = await axios.get(apiUrl);
+    const videoUrl = response.data.url;
 
-    // استخدم الAPI الجديدة للحصول على صورة أنمي عشوائية
-    const apiUrl = "https://anime.grykj.repl.co";
-    const response = await openai.createImage({
-      prompt: apiUrl,
-      n: 1,
-      size: "1024x1024",
-    });
-    const image_url = response.data.data[0].url;
-    const imagePath = path.join(__dirname, '../temp/random_anime.jpg');
-
-    https.get(image_url, (linkResponse) => {
-      const fileStream = fs.createWriteStream(imagePath);
-      linkResponse.pipe(fileStream);
-      fileStream.on('finish', () => {
-        console.log("Anime image saved!");
-        var message = {
-          body: " ✨ | إليك صور الأنمي اللتي طلبت:",
-          attachment: fs.createReadStream(imagePath)
-        };
-        api.sendMessage(message, event.threadID, event.messageID);
-      });
-    });
+    api.sendMessage({
+      body: 'فيديو أنمي:',
+      attachment: videoUrl,
+    }, event.threadID, event.messageID);
   } catch (error) {
-    console.log(error);
-    api.sendMessage(" ❌ | حدث خطأ أثناء جلب البيانات من واجهة برمجة التطبيقات حاول مجددا لاحقا.", event.threadID, event.messageID);
+    api.sendMessage(`❌ | حدث خطأ أثناء جلب مقطع الأنمي. الخطأ: ${error.message}`, event.threadID);
   }
 }
 
-module.exports = randomAnime;
+module.exports = getAnimeClip;
